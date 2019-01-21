1958e6f8d5e94fdb4fb516ea59a497ec4ace4021
frin88 <francesca.bagni@gmail.com>
2019-01-21 15:56:18 +0100

    fixed layout ref

--- layout/index.tpl.php
+++ layout/index.tpl.php
@@ -13,11 +13,11 @@
     <!-- Bootstrap core CSS --
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
      -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+      <link href="../public/css/bootstrap.css" rel="stylesheet">
    
 
     <!-- Custom styles for this template -->
-    <link href="/css/starter-template.css" rel="stylesheet">
+    <link href="../public/css/starter-template.css" rel="stylesheet">
   </head>
 
   <body>
@@ -46,7 +46,7 @@
     </nav>
 
     <div class="container">
-    <?= $this->content; ?>
+
     
 
     </div><!-- /.container -->
@@ -57,9 +57,6 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="../public/js/bootstrap.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
   </body>
 </html>
